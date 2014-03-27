38c0e06d2ded19298d9d97999cdd4d2696a02778
Ben Niu <benniu.618@gmail.com>
2014-03-26 21:49:57 -0500

    finish database tryout

---
+++ app/views/comments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @comment, :id, :details, :event_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 LoveCave::Application.routes.draw do
+  resources :comments
+
+  resources :events
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
